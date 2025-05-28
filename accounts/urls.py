a9ba22807115938d6badd52ac34fff33c25c9e18
Mirikrom <tvs730818@gmail.com>
2025-05-28 11:50:21 +0500

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', views.register, name='register'),
-    path('google-login/', views.google_login, name='google_login'),  # qo'shildi
+    path('google-login/', views.google_login, name='google_login'), 
 ]
